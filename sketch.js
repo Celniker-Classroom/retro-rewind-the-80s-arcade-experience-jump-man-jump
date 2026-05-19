b29a53fa40622b8e0d44e9ccef59cb3f9124ccac
RoxtarKidd <132388317+RoxtarKidd@users.noreply.github.com>
2026-05-19 20:34:56 +0000

    Trying to adgrassd

--- sketch.js
+++ sketch.js
@@ -5,21 +5,38 @@ let ball = new Sprite();
 ball.diameter = 50;
 ball.img = '🤪';
 
-let groundA = new Sprite();
-groundA.x = -120;
-groundA.width = 220;
-groundA.rotation = 30;
-groundA.physics = STATIC;
+let grass = new Group();
+grass.d = 24;
+grass.img = 'images/grassBlock.png';
+grass.tile = 'g';
+grass.physics = STATIC;
 
-let groundB = new Sprite();
-groundB.x = 120;
-groundB.width = 220;
-groundB.rotation = -30;
-groundB.physics = STATIC;
+let level = new Group();
+
+level.addTiles(`
+                                
+gg                                      g
+
+   gg
+                                   g
+      ggg                        g
+                             g                
+           ggg                                
+                           
+ggggggggggg   ggggg   gggggggggggg            ggg
+`);
+
+tile.physics = STATIC;
 
 q5.update = function () {
 	background('skyblue');
 	text('click to jump!', 0, -50);
 
 	if (mouse.presses()) ball.vel.y = -5;
+	if (keyIsPressed && (key === 'w')) {
+		ball.vel.y = -5;
+	}
+	if (keyIsPressed && (key === 's')) {
+		ball.vel.y = 5;
+	}
 };
