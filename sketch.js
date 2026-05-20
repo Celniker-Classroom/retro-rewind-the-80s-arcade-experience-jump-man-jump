a03923a52a4548114c359d74d3e4a6485f19bba4
RoxtarKidd <132388317+RoxtarKidd@users.noreply.github.com>
2026-05-20 20:55:04 +0000

    Fixed controls

--- sketch.js
+++ sketch.js
@@ -1,42 +1,41 @@
-await Canvas();
-world.gravity.y = 10;
+// 1. Create a visible drawing board with size parameters
+await Canvas(); 
+world.gravity.y = 0;
 
 let ball = new Sprite();
+ball.x = 400; // Place the ball over the ground
+ball.y = 100;
 ball.diameter = 50;
 ball.img = '🤪';
 
-let grass = new Group();
-grass.d = 24;
-grass.img = 'images/grassBlock.png';
-grass.tile = 'g';
-grass.physics = STATIC;
+// 2. Define your tile blueprint properties
+let grass = new Sprite();
 
-let level = new Group();
+grass.w = 8; 
+grass.h = 8;
+grass.img = 'images/Block1.png';
+grass.scale.x = 5;
+grass.scale.y = 5;
+grass.x = 0;
+grass.y = 100;
+grass.physics = STATIC; 
 
-level.addTiles(`
-                                
-gg                                      g
-
-   gg
-                                   g
-      ggg                        g
-                             g                
-           ggg                                
-                           
-ggggggggggg   ggggg   gggggggggggg            ggg
-`);
-
-tile.physics = STATIC;
+// 3. Create a level group container to spawn your level layout map
 
 q5.update = function () {
 	background('skyblue');
-	text('click to jump!', 0, -50);
+	
+	// Keep the camera locked on your player
 
-	if (mouse.presses()) ball.vel.y = -5;
-	if (keyIsPressed && (key === 'w')) {
-		ball.vel.y = -5;
-	}
-	if (keyIsPressed && (key === 's')) {
-		ball.vel.y = 5;
-	}
+	// Display UI text relative to player position
+	text('click or press W / S to jump!', ball.x - 70, ball.y - 50);
+
+	// Controls
+	if (kb.pressing('left')) player.vel.x = -5;
+	else if (kb.pressing('right')) player.vel.x = 5;
+	else player.vel.x = 0;
+
+	if (kb.pressing('up')) player.vel.y = -5;
+	else if (kb.pressing('down')) player.vel.y = 5;
+	else player.vel.y = 0;
 };
